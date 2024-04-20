80acf8fa129681f3649ddd9cb25e79e8eb551f44
WittyCo <witty.ysl@gmail.com>
2024-04-20 23:00:42 +0800

    Add venv-selector for select python virtual env

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -188,4 +188,23 @@ return {
       },
     },
   },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = {
+      -- Your options go here
+      -- name = "venv",
+      auto_refresh = false,
+      anaconda_base_path = "/opt/anaconda3",
+      anaconda_envs_path = "/opt/anaconda3/envs",
+    },
+    lazy = true,
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Select Python Venv" },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Select Python Venv(Cached)" },
+    },
+  },
 }
