9bcaf03d5b3d8dc7b381b0527730ae29eb2e8648
WittyCo <witty.ysl@gmail.com>
2024-05-14 20:58:16 +0800

    Enable inlay hints.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,7 +31,7 @@ return {
     end,
     ---@class PluginLspOpts
     opts = {
-      inlay_hints = { enabled = false },
+      inlay_hints = { enabled = true },
       ---@type lspconfig.options
       servers = {
         clangd = {
