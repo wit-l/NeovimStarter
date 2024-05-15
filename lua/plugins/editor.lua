daa11f8650d039d24be6cfba58368da454975ed4
WittyCo <witty.ysl@gmail.com>
2024-05-15 14:54:01 +0800

    Update dependency.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,6 +2,10 @@ return {
   {
     "telescope.nvim",
     dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
       "nvim-telescope/telescope-file-browser.nvim",
     },
     keys = {
