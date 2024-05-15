87201b122b25b4fc8de644499bdad344239ec1b2
WittyCo <witty.ysl@gmail.com>
2024-05-15 19:08:50 +0800

    Add rest-nvim to perform many API requests.

---
+++ lua/plugins/integrations.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    },
+  },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("rest-nvim").setup()
+    end,
+    keys = {
+      {
+        "\\r",
+        "<cmd>Rest run<cr>",
+        desc = "Test the current file",
+      },
+    },
+  },
+}
