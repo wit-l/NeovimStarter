d0976f4f7c15456e2fab00bddc53d871015c3751
wit-l <witty.ysl@gmail.com>
2025-03-08 01:55:54 +0800

    Replace rest.nvim with kulala.nvim

--- lua/plugins/integrations.lua
+++ lua/plugins/integrations.lua
@@ -1,25 +1,69 @@
 return {
+  --   {
+  --     "vhyrro/luarocks.nvim",
+  --     priority = 1000,
+  --     config = true,
+  --     lazy = true,
+  --     opts = {
+  --       rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+  --     },
+  --   },
+  --   {
+  --     "rest-nvim/rest.nvim",
+  --     lazy = true,
+  --     ft = "http",
+  --     event = "BufEnter *.http",
+  --     dependencies = { "luarocks.nvim" },
+  --     keys = {
+  --       {
+  --         "\\r",
+  --         "<cmd>Rest run<cr>",
+  --         desc = "Test the current file",
+  --       },
+  --     },
+  --   },
   {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true,
-    lazy = true,
-    opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-    },
-  },
-  {
-    "rest-nvim/rest.nvim",
-    lazy = true,
-    ft = "http",
-    event = "BufEnter *.http",
-    dependencies = { "luarocks.nvim" },
+    "mistweaverco/kulala.nvim",
     keys = {
       {
-        "\\r",
-        "<cmd>Rest run<cr>",
-        desc = "Test the current file",
+        "\\s",
+        function()
+          require("kulala").run()
+        end,
+        mode = { "n", "v" },
+        desc = "Send request",
+        ft = { "http", "rest" },
       },
+      {
+        "\\a",
+        function()
+          require("kulala").run_all()
+        end,
+        mode = { "n", "v" },
+        desc = "Send all requests",
+        ft = "http",
+      },
+      {
+        "<leader>Ra",
+        function()
+          require("kulala").run_all()
+        end,
+        desc = "Send all requests",
+        ft = { "http", "rest" },
+      },
+      {
+        "<leader>Ro",
+        function()
+          require("kulala").open()
+        end,
+        desc = "Open kulala",
+        ft = { "http", "rest" },
+      },
+    },
+    ft = { "http", "rest" },
+    opts = {
+      -- your configuration comes here
+      global_keymaps = true,
     },
   },
 }
