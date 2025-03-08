74c2a93f8a7d65dbbd6168c00690a70e04a07be9
wit-l <witty.ysl@gmail.com>
2025-03-08 15:15:03 +0800

    Enable REST-Client Interface with kulala.nvim

--- lua/plugins/integrations.lua
+++ lua/plugins/integrations.lua
@@ -1,27 +1,46 @@
 return {
   {
-    "vhyrro/luarocks.nvim",
-    enabled = false,
-    priority = 1000,
-    config = true,
-    lazy = true,
-    opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-    },
-  },
-  {
-    "rest-nvim/rest.nvim",
-    lazy = true,
-    ft = "http",
-    enabled = false,
-    event = "BufEnter *.http",
-    dependencies = { "luarocks.nvim" },
+    "mistweaverco/kulala.nvim",
     keys = {
       {
-        "\\r",
-        "<cmd>Rest run<cr>",
-        desc = "Run request under the cursor",
+        "\\s",
+        function()
+          require("kulala").run()
+        end,
+        mode = { "n", "v" },
+        desc = "Send request",
+        ft = { "http", "rest" },
       },
+      {
+        "\\a",
+        function()
+          require("kulala").run_all()
+        end,
+        mode = { "n", "v" },
+        desc = "Send all requests",
+        ft = "http",
+      },
+      {
+        "<leader>Ra",
+        function()
+          require("kulala").run_all()
+        end,
+        desc = "Send all requests",
+        ft = { "http", "rest" },
+      },
+      {
+        "<leader>Ro",
+        function()
+          require("kulala").open()
+        end,
+        desc = "Open kulala",
+        ft = { "http", "rest" },
+      },
+    },
+    ft = { "http", "rest" },
+    opts = {
+      -- your configuration comes here
+      global_keymaps = true,
     },
   },
 }
