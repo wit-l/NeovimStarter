7d0fd2a1d8de96979d6dd9a6bc5267c0f9ca1f8b
wit-l <witty.ysl@gmail.com>
2025-05-24 15:56:30 +0800

    Set previewr of git diff to delta in Snacks.picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -150,6 +150,7 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    ---@type snacks.Config
     opts = {
       bigfile = { enabled = true },
       notifier = { enabled = true },
@@ -171,6 +172,14 @@ return {
             pane = 2,
             { section = "keys", gap = 1, padding = 1 },
             { section = "startup" },
+          },
+        },
+      },
+      picker = {
+        previewers = {
+          diff = {
+            builtin = false,
+            cmd = "delta",
           },
         },
       },
