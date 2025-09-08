56edcf98848baaf6c121a0a2bc60a33bf0218b6e
wit-l <witty.ysl@gmail.com>
2025-09-08 11:16:56 +0800

    feat: Transparent floating window

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,6 +9,9 @@ return {
     priority = 1000,
     opts = {
       transparent_background = true,
+      float = {
+        transparent = true, -- enable transparent floating windows
+      },
     },
   },
 }
