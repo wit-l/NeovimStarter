6da49645abac38d36d701eb621af08606f64caec
wit-l <witty.ysl@gmail.com>
2025-09-10 10:54:07 +0800

    Remove a comment

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -227,7 +227,6 @@ return {
   {
     "saghen/blink.cmp",
     opts = function(_, opts)
-      -- 安全地合并边框配置到默认配置中
       opts.completion = vim.tbl_deep_extend("force", opts.completion or {}, {
         menu = { border = "rounded" },
         documentation = { window = { border = "rounded" } },
