497e951c0736dce00dd62f6bcc82332a6c04c406
wit-l <witty.ysl@gmail.com>
2025-09-11 14:33:50 +0800

    Show bufferline close icons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -42,8 +42,8 @@ return {
     opts = {
       options = {
         mode = "tabs",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
+        -- show_buffer_close_icons = false,
+        -- show_close_icon = false,
       },
     },
   },
