85de8794c9be274d3e7e8af2a9c79b202fed5c4d
wit-l <witty.ysl@gmail.com>
2025-09-11 14:31:42 +0800

    Use default icons in completion menu

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -232,26 +232,26 @@ return {
           draw = {
             treesitter = { "lsp" },
             columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
-            components = {
-              kind_icon = {
-                text = function(ctx)
-                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                  return kind_icon
-                end,
-                -- (optional) use highlights from mini.icons
-                highlight = function(ctx)
-                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                  return hl
-                end,
-              },
-              kind = {
-                -- (optional) use highlights from mini.icons
-                highlight = function(ctx)
-                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                  return hl
-                end,
-              },
-            },
+            -- components = {
+            --   kind_icon = {
+            --     text = function(ctx)
+            --       local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+            --       return kind_icon
+            --     end,
+            --     -- (optional) use highlights from mini.icons
+            --     highlight = function(ctx)
+            --       local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+            --       return hl
+            --     end,
+            --   },
+            --   kind = {
+            --     -- (optional) use highlights from mini.icons
+            --     highlight = function(ctx)
+            --       local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+            --       return hl
+            --     end,
+            --   },
+            -- },
           },
         },
         documentation = { window = { border = "rounded" } },
