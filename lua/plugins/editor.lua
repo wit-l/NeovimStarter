683f180ebb1964921193567200489f9f363f11a2
wit-l <witty.ysl@gmail.com>
2025-09-14 00:14:14 +0800

    refactor: Optimise some codes

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -152,12 +152,12 @@ return {
                 vim.cmd("startinsert")
               end,
               ["<C-u>"] = function(prompt_bufnr)
-                for i = 1, 10 do
+                for _ = 1, 10 do
                   actions.move_selection_previous(prompt_bufnr)
                 end
               end,
               ["<C-d>"] = function(prompt_bufnr)
-                for i = 1, 10 do
+                for _ = 1, 10 do
                   actions.move_selection_next(prompt_bufnr)
                 end
               end,
