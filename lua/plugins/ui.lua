2d7ad84042bd799f221422d75587015aa7df9a33
wit-l <witty.ysl@gmail.com>
2025-09-15 09:42:25 +0800

    Change image preview tool to viu (from chafa) in the dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -146,7 +146,8 @@ return {
         sections = {
           {
             section = "terminal",
-            cmd = "chafa ~/.config/pic/t1.png --format symbols --symbols vhalf --size 60x20 --stretch; sleep .1",
+            -- cmd = "chafa ~/.config/pic/t1.png --format symbols --symbols vhalf --size 60x20 --stretch; sleep .1",
+            cmd = "viu ~/.config/pic/t1.png -w 60 -h 20; sleep .1",
             height = 20,
             padding = 1,
           },
