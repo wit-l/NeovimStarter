27fb7d28b6a347810b4e0355a6cc316507851505
lsy <986448807@qq.com>
2026-07-26 22:50:36 +0800

    更改bufferline模式为buffers

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -23,15 +23,11 @@ return {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
     opts = {
       options = {
-        mode = "tabs",
-        -- show_buffer_close_icons = false,
-        -- show_close_icon = false,
+        mode = "buffers",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
       },
     },
   },
