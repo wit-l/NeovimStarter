3b5088f7ac05416abf0786adeeaea0a25fb7e81b
lsy <986448807@qq.com>
2026-07-26 22:34:23 +0800

    refactor: 将mason中错误指定安装lsp等工具的操作移动到mason-lspconfig插件中

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,20 @@
+---@module "mason"
+---@module "mason-lspconfig"
+---@module "lazy"
+---@type LazySpec
 return {
   {
     "mason-org/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "html-lsp",
-        "css-lsp",
-        "emmet-language-server",
-        "pyright",
-        "clangd",
-      })
-      opts.ui = {
+    ---@type MasonSettings
+    opts = {
+      pip = {
+        upgrade_pip = false,
+        install_args = { "-i", "https://pypi.tuna.tsinghua.edu.cn/simple" },
+      },
+      npm = {
+        install_args = { "--registry", "https://registry.npmmirror.com" },
+      },
+      ui = {
         border = "rounded",
         backdrop = 100, -- The backdrop opacity
         icons = {
@@ -24,12 +22,49 @@ return {
           package_pending = "➜",
           package_uninstalled = "✗",
         },
-      }
-    end,
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    ---@type MasonLspconfigSettings
+    opts = {
+      ensure_installed = {
+        "bashls",
+        "biome",
+        "clangd",
+        "cssls",
+        "docker_compose_language_service",
+        "dockerls",
+        "emmet_language_server",
+        "eslint",
+        "docker_language_server",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "marksman",
+        "oxfmt",
+        "oxlint",
+        "pyright",
+        "ruff",
+        "stylua",
+        "tailwindcss",
+        "taplo",
+        "texlab",
+        "ts_ls",
+        "vtsls",
+        "vue_ls",
+        "yamlls",
+      },
+      automatic_enable = true,
+    },
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
   },
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
       inlay_hints = {
         enabled = false,
