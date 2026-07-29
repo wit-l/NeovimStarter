a1f604c5ca31b9457077aa9119e2459b4cd23a14
lsy <986448807@qq.com>
2026-07-29 23:02:28 +0800

    浮动终端隐藏快捷键更改为`ctrl+q`

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -257,7 +257,7 @@ return {
         toggle_manager = "<C-\\>",
         prev_session = "<C-k>",
         next_session = "<C-j>",
-        hide_terminal = "<C-[>",
+        hide_terminal = "<C-q>",
       },
     },
   },
