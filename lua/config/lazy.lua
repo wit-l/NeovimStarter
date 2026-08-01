e89cfd31b18b1ecf12a469c059d69c61449253e8
wit-l <986448807@qq.com>
2026-08-01 17:58:24 +0800

    取消禁用gzip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -57,7 +57,7 @@ require("lazy").setup({
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        "gzip",
+        -- "gzip",
         -- "matchit",
         -- "matchparen",
         "netrwPlugin",
