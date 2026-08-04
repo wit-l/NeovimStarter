f612c4c65ac7fee1ac6e48e0f48a3153b7672a47
lsy <lsy@test.com>
2026-08-04 12:38:19 +0800

    使用windwp/nvim-autopairs替代mini.pairs以实现html标签配对
    主要实现在html标签中回车时让开&闭标签分别分布在当前缩进上的第一三行，光标放在第二行

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -52,4 +52,13 @@ return {
     "folke/flash.nvim",
     pin = true,
   },
+  {
+    "nvim-mini/mini.pairs",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
 }
