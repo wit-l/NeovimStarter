0522686b0abff2285e86f3fdc29e72562f561513
wit-l <986448807@qq.com>
2026-08-16 23:27:21 +0800

    fix: 修复在jsx标签中写入泛型后插入>时错误地提早补全反标签的问题

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,37 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
+  -- LazyVim installs this with opts={}; wrap close_tag so `>` of TS generics
+  -- like <Table<IDataType> does not insert </IDataType> or a premature </Table>.
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+    config = function(_, opts)
+      require("nvim-ts-autotag").setup(opts)
+      local internal = require("nvim-ts-autotag.internal")
+      local close_tag = internal.close_tag
+      internal.close_tag = function()
+        local ft = vim.bo.filetype
+        if ft == "typescriptreact" or ft == "javascriptreact" or ft == "typescript" then
+          local line = vim.api.nvim_get_current_line()
+          local col = vim.api.nvim_win_get_cursor(0)[2]
+          local before = line:sub(1, col)
+          if before:match("[%w_%.]<%s*[^<>%s][^<>]*$") then
+            return
+          end
+          local ok, node = pcall(vim.treesitter.get_node)
+          while ok and node do
+            local t = node:type()
+            if t == "type_arguments" or t == "type_parameters" then
+              return
+            end
+            node = node:parent()
+          end
+        end
+        return close_tag()
+      end
+    end,
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
