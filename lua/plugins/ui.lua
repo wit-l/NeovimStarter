15e5c173cc9ae0ef80f096060bea3aef940e281e
IanLu <986448807@qq.com>
2026-08-23 02:39:26 +0800

    fix(law): 上取消通过`ctrl+[`进入nvim的normal模式

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -503,7 +503,7 @@ return {
           desc = "Double escape to normal mode",
         }
         vim.keymap.set("t", "<Esc>", on_escape, esc_opts)
-        vim.keymap.set("t", "<C-[>", on_escape, esc_opts)
+        -- vim.keymap.set("t", "<C-[>", on_escape, esc_opts)
       end
 
       events.on(events.SESSION_CREATED, function(payload)
